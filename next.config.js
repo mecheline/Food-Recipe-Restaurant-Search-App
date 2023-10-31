5754c0882d2a5b0c359e5f6a0e973d2c16730e90
mecheline <ucheamaechi2016@gmail.com>
2023-10-31 12:26:37 +0100

    nodemailer issue fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   domains: {
     images: ["edamam-product-images.s3.amazonaws.com"],
   },
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,6 +2,7 @@ import connectDB from "@/lib/db";
 import pendingUser from "@/models/pendingUser";
 import User from "@/models/user";
 import { confirmationLinkEmail } from "@/nodemailer/mail";
+import { Resend } from "resend";
 
 import { hash } from "bcrypt";
 
@@ -51,7 +52,17 @@ export default async function signup(req, res) {
         resetToken: token,
       });
       const result = await newUser.save();
-      confirmationLinkEmail(result.resetToken, result.email);
+
+      // const resend = new Resend(process.env.RESEND);
+
+      // resend.emails.send({
+      //   from: process.env.USER,
+      //   to: result.email,
+      //   subject: "Verify Email Link",
+      //   html: `<a target="_" href="${process.env.NEXTAUTH_URL}/verify-email/${result.resetToken}">Click on the link to confirm your email</a>`,
+      // });
+
+      await confirmationLinkEmail(result.resetToken, result.email);
       res.status(200).json({
         message: "Check your email to activate your account",
         // result,
